7ba83a6f19ed4ffd9c70c02510540d862e0aa245
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-04-07 20:25:56 -0300

    first commit

--- labels.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Label(Enum):
-    HAS_PROFESSION = "HAS_PROFESSION"
-    NO_PROFESSION = "NO_PROFESSION"
-    NOT_SET = "LABEL_IS_NOT_SET"
--- relations.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Relation(Enum):
-    MARRIED_WITH = "MARRIED_WITH"
-    BROTHER_OF = "BROTHER_OF"
-    DATE_WITH = "DATE_WITH"
-    NO_RELATION = "NO_RELATION"
